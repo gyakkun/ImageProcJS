8b2f9d6ffa663cf0e8351de95e1b4857f82caa43
gyakkun <xiaowufeixia@gmail.com>
2018-09-30 21:52:34 +0800

    Package some procedures into functions.

--- algorithms/my_mediancut.js
+++ algorithms/my_mediancut.js
@@ -32,16 +32,44 @@
 }
 */
 
-// function sumColor
+function getColorArrayFromImage(srcImg){
+    var max = {r: 0, g: 0, b: 0};
+    var min = {r: 255, g: 255, b: 255};
+    var colorBack = [];
+    var palette = [];
+
+    for (var outer = 0; outer < srcImg.w; outer++) {
+        for (var inner = 0; inner < srcImg.h; inner++) {
+            var tmpColor = srcImg.getPixel(outer, inner);
+            
+            max.r = Math.max(tmpColor.r, max.r);
+            max.g = Math.max(tmpColor.g, max.g);
+            max.b = Math.max(tmpColor.b, max.b);
+            
+            min.r = Math.min(tmpColor.r, min.r);
+            min.g = Math.min(tmpColor.g, min.g);
+            min.b = Math.min(tmpColor.b, min.b);
+            
+            //Stringfy the color, add weight in palette
+            var tmpHex = rgb2hex(tmpColor);
+            if (palette[tmpHex]) {
+                palette[tmpHex]++;
+            } else {
+                palette[tmpHex] = 1;
+            }
+            //console.log(tmpHex);
+            //sleep(100);
+            colorBack.push(tmpColor);
+        }
+    }
+    
+    return colorBack;
+}
 
 function getMeanColor(colorArray){
     var len = colorArray.length;
     var origLen = colorArray.length;
-    var colorBack = {
-        r : 0,
-        g : 0,
-        b : 0,
-    };
+    var colorBack = {r:0, g:0, b:0};
     while(len--) {
         colorBack.r += colorArray[len].r;
         colorBack.g += colorArray[len].g;
@@ -65,30 +93,15 @@ function sleep(numMs) {
     }
 }
 
-// @param src : source image obj
-// @param n   : target num of colors
-// @return    : target color array obj
-
-function mediancut(src, n) {
+function split(colorArray){
     var maxRangeColor="r";
-    var palette = {};
     var colorBack = [];
-    var max = {
-        r: 0,
-        g: 0,
-        b: 0
-    };
+    var max = {r: 0,g: 0,b: 0};
+    var min = {r: 255,g: 255,b: 255};
     
-    var min = {
-        r: 255,
-        g: 255,
-        b: 255
-    };
-
-    for (var outer = 0; outer < src.w; outer++) {
-        for (var inner = 0; inner < src.h; inner++) {
-            var tmpColor = src.getPixel(outer, inner);
-            
+    //console.log(src.getPixel(1,1));   //RGBA 4 channels
+    colorArray.forEach(function(tmpColor){
+        
             max.r = Math.max(tmpColor.r, max.r);
             max.g = Math.max(tmpColor.g, max.g);
             max.b = Math.max(tmpColor.b, max.b);
@@ -97,17 +110,11 @@ function mediancut(src, n) {
             min.g = Math.min(tmpColor.g, min.g);
             min.b = Math.min(tmpColor.b, min.b);
             
-            var tmpHex = rgb2hex(tmpColor);
-            if (palette[tmpHex]) {
-                palette[tmpHex]++;
-            } else {
-                palette[tmpHex] = 1;
-            }
             //console.log(tmpHex);
             //sleep(100);
             colorBack.push(tmpColor);
-        }
-    }
+    })
+
     //console.log(palette);
     
     range = {
@@ -140,19 +147,35 @@ function mediancut(src, n) {
     var colorLeft = colorBack.slice(0,colorBack.length/2);
     var colorRight = colorBack.slice(colorBack.length/2);
     
-    //console.log(colorLeft);
-    //console.log(colorRight);
-    
-    console.log(getMeanColor(colorLeft),getMeanColor(colorRight));
-    
-    var lc = getMeanColor(colorLeft);
-    var rc = getMeanColor(colorRight);
+    return {
+        left : colorLeft,
+        right: colorRight
+    }
+}
 
+// @param src : source image obj
+// @param n   : target num of colors
+// @return    : target color array obj
+
+function mediancut(src, n) {
+    var numLoop = parseInt(Math.log(n)/Math.log(2));
+    var trueN = Math.pow(2,numLoop);
+    var colorMap = [];
+    var colorBack = [];
+    var twoBox = {};
     
-    colorBack = [];
-    colorBack.push(lc);
-    colorBack.push(rc);
+    colorBack = getColorArrayFromImage(src);
+    twoBox = split(colorBack);
     
-    return colorBack;
+    console.log(getMeanColor(twoBox.left),getMeanColor(twoBox.right));
+    
+    var lc = getMeanColor(twoBox.left);
+    var rc = getMeanColor(twoBox.right);
+    
+    colorMap = [];
+    colorMap.push(lc);
+    colorMap.push(rc);
+    
+    return colorMap;
 
 }
